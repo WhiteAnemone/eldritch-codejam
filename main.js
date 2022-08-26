f553a1d862b9fb83c3fe621b1af68bac5036185b
WhiteAnemone <99764833+WhiteAnemone@users.noreply.github.com>
2022-08-27 00:49:30 +0300

    feat: add html, js and css

---
+++ main.js
@@ -0,0 +1,273 @@
+// const demon = document.querySelector('.demon')
+// const ancients = ['azathoth', 'cthulhu', 'iogSothoth', 'shubNiggurath']
+// const random = Math.floor(Math.random() * ((ancients.length-1)- 0 + 1) + 0);
+
+//import ancients from "./assets/Ancients/index.js";
+// import ancientsData from "./data/ancients.js";
+//import { brownCards } from "./mythicCards.js";
+
+
+// const img = new Image();
+//     img.src = 'https://github.com/WhiteAnemone/eldritch-codejam/blob/main/assets/Ancients/Azathoth.png'
+//     img.onload = () => {
+//         body.style.backgroundImage = `url(${img.src}`;
+//     } 
+// demon.textContent = ancients[random]
+
+// console.log(random)
+
+// const checkFunction = () => {
+//     shuffle.style.display = 'block'
+// }
+
+
+
+// Easy green cards
+const green1 = new Image();
+green1.src = 'assets/MythicCards/green/green1.png'
+const green12 = new Image();
+green12.src = 'assets/MythicCards/green/green12.png'
+const green16 = new Image();
+green16.src = 'assets/MythicCards/green/green16.png'
+const green17 = new Image();
+green17.src = 'assets/MythicCards/green/green17.png'
+const green18 = new Image();
+green18.src = 'assets/MythicCards/green/green18.png'
+
+// Normal green cards
+const green7 = new Image();
+green7.src = 'assets/MythicCards/green/green7.png'
+const green8 = new Image();
+green8.src = 'assets/MythicCards/green/green8.png'
+const green9 = new Image();
+green9.src = 'assets/MythicCards/green/green9.png'
+const green10 = new Image();
+green10.src = 'assets/MythicCards/green/green10.png'
+const green11 = new Image();
+green11.src = 'assets/MythicCards/green/green11.png'
+const green13 = new Image();
+green13.src = 'assets/MythicCards/green/green13.png'
+const green14 = new Image();
+green14.src = 'assets/MythicCards/green/green14.png'
+const green15 = new Image();
+green15.src = 'assets/MythicCards/green/green15.png'
+
+// Hard green cards
+const green2 = new Image();
+green2.src = 'assets/MythicCards/green/green2.png'
+const green3 = new Image();
+green3.src = 'assets/MythicCards/green/green3.png'
+const green4 = new Image();
+green4.src = 'assets/MythicCards/green/green4.png'
+const green5 = new Image();
+green5.src = 'assets/MythicCards/green/green5.png'
+const green6 = new Image();
+green6.src = 'assets/MythicCards/green/green6.png'
+
+const greenCards = [green1.src, green2.src, green3.src, green4.src, green5.src, green6.src, green7.src, green8.src, green9.src, green10.src, green11.src, green12.src, green13.src, green14.src, green15.src, green16.src, green17.src, green18.src];
+
+// Easy brown cards
+const brown11 = new Image();
+brown11.src = 'assets/MythicCards/brown/brown11.png'
+const brown12 = new Image();
+brown12.src = 'assets/MythicCards/brown/brown12.png'
+const brown13 = new Image();
+brown13.src = 'assets/MythicCards/brown/brown13.png'
+const brown14 = new Image();
+brown14.src = 'assets/MythicCards/brown/brown14.png'
+const brown21 = new Image();
+brown21.src = 'assets/MythicCards/brown/brown21.png'
+
+// Normal brown cards
+const brown1 = new Image();
+brown1.src = 'assets/MythicCards/brown/brown1.png'
+const brown2 = new Image();
+brown2.src = 'assets/MythicCards/brown/brown2.png'
+const brown3 = new Image();
+brown3.src = 'assets/MythicCards/brown/brown3.png'
+const brown4 = new Image();
+brown4.src = 'assets/MythicCards/brown/brown4.png'
+const brown5 = new Image();
+brown5.src = 'assets/MythicCards/brown/brown5.png'
+const brown15 = new Image();
+brown15.src = 'assets/MythicCards/brown/brown15.png'
+const brown16 = new Image();
+brown16.src = 'assets/MythicCards/brown/brown16.png'
+const brown17 = new Image();
+brown17.src = 'assets/MythicCards/brown/brown17.png'
+const brown18 = new Image();
+brown18.src = 'assets/MythicCards/brown/brown18.png'
+const brown19 = new Image();
+brown19.src = 'assets/MythicCards/brown/brown19.png'
+const brown20 = new Image();
+brown20.src = 'assets/MythicCards/brown/brown20.png'
+
+// Hard brown cards
+const brown6 = new Image();
+brown6.src = 'assets/MythicCards/brown/brown6.png'
+const brown7 = new Image();
+brown7.src = 'assets/MythicCards/brown/brown7.png'
+const brown8 = new Image();
+brown8.src = 'assets/MythicCards/brown/brown8.png'
+const brown9 = new Image();
+brown9.src = 'assets/MythicCards/brown/brown9.png'
+const brown10 = new Image();
+brown10.src = 'assets/MythicCards/brown/brown10.png'
+
+const brownCards = [brown1.src, brown2.src, brown3.src, brown4.src, brown5.src, brown6.src, brown7.src, brown8.src, brown9.src, brown10.src, brown11.src, brown12.src, brown13.src, brown14.src, brown15.src, brown16.src, brown17.src, brown18.src, brown19.src, brown20.src, brown21.src];
+
+// Easy blue cards
+const blue3 = new Image();
+blue3.src = 'assets/MythicCards/blue/blue3.png'
+const blue4 = new Image();
+blue4.src = 'assets/MythicCards/blue/blue4.png'
+const blue5 = new Image();
+blue5.src = 'assets/MythicCards/blue/blue5.png'
+const blue10 = new Image();
+blue10.src = 'assets/MythicCards/blue/blue10.png'
+
+// Normal blue cards
+const blue7 = new Image();
+blue7.src = 'assets/MythicCards/blue/blue7.png'
+const blue9 = new Image();
+blue9.src = 'assets/MythicCards/blue/blue9.png'
+const blue11 = new Image();
+blue11.src = 'assets/MythicCards/blue/blue11.png'
+const blue12 = new Image();
+blue12.src = 'assets/MythicCards/blue/blue12.png'
+
+// Hard blue cards
+const blue1 = new Image();
+blue1.src = 'assets/MythicCards/blue/blue1.png'
+const blue2 = new Image();
+blue2.src = 'assets/MythicCards/blue/blue2.png'
+const blue6 = new Image();
+blue6.src = 'assets/MythicCards/blue/blue6.png'
+const blue8 = new Image();
+blue8.src = 'assets/MythicCards/blue/blue8.png'
+
+// const blueCardsEasy = [blue3.src, blue4.src, blue5.src, blue10.src];
+// const blueCardsNormal = [blue7.src, blue9.src, blue11.src, blue12.src];
+// const blueCardsHard = [blue1.src, blue2.src, blue6.src, blue8.src];
+// const blueCards = [blueCardsEasy, blueCardsNormal, blueCardsHard];
+
+const blueCards = [blue1.src, blue2.src, blue3.src, blue4.src, blue5.src, blue6.src, blue7.src, blue8.src, blue9.src, blue10.src, blue11.src, blue12.src];
+
+// Const
+const normal = document.querySelector('.normal');
+const shuffle = document.querySelector('.shuffle');
+const stages = document.querySelector('.stages');
+const bcgCard = document.querySelector('.bcgCard');
+const cards = document.querySelector('.cards');
+const azathoth = document.querySelector('.azathoth');
+const greenCircle = document.querySelectorAll('.circle-green');
+const brownCircle = document.querySelectorAll('.circle-brown');
+const blueCircle = document.querySelectorAll('.circle-blue');
+
+let difficulty = '';
+let greenCardsAmount = 0;
+let brownCardsAmount = 0;
+let blueCardsAmount = 0;
+
+for (let elem of greenCircle) {
+    greenCardsAmount = greenCardsAmount + +elem.textContent;
+}
+
+for (let elem of brownCircle) {
+    brownCardsAmount = brownCardsAmount + +elem.textContent;
+}
+
+for (let elem of blueCircle) {
+    blueCardsAmount = blueCardsAmount + +elem.textContent;
+}
+
+// console.log(greenCardsAmount, brownCardsAmount, blueCardsAmount)
+
+
+// New decks
+let randomNumber = 0;
+let newGreenDeck = [];
+let newBrownDeck = [];
+let newBlueDeck = [];
+
+for (let i = 0; i < greenCardsAmount; i++) {
+    randomNumber = Math.floor(Math.random() * ((greenCards.length - 1) - 0 + 1) + 0);
+    let a = greenCards.indexOf(greenCards[randomNumber]);
+    newGreenDeck.push(greenCards[randomNumber])
+    if (a >= 0) {
+        greenCards.splice(a, 1)
+    }
+}
+
+for (let i = 0; i < brownCardsAmount; i++) {
+    randomNumber = Math.floor(Math.random() * ((brownCards.length - 1) - 0 + 1) + 0);
+    let a = brownCards.indexOf(brownCards[randomNumber]);
+    newBrownDeck.push(brownCards[randomNumber])
+    if (a >= 0) {
+        brownCards.splice(a, 1)
+    }
+}
+
+for (let i = 0; i < blueCardsAmount; i++) {
+    randomNumber = Math.floor(Math.random() * ((blueCards.length - 1) - 0 + 1) + 0);
+    let a = blueCards.indexOf(blueCards[randomNumber]);
+    newBlueDeck.push(blueCards[randomNumber])
+    if (a >= 0) {
+        blueCards.splice(a, 1)
+    }
+}
+
+console.log(newGreenDeck)
+console.log(newBrownDeck)
+console.log(newBlueDeck)
+
+
+
+// Search algorithm
+// let newDeck = [];
+// let randomDifficulty = 0;
+// let randomCard = 0;
+
+// bcgCard.addEventListener('click', () => {
+//     cards.style.visibility = 'visible'
+//     if (difficulty == 'normal') {
+//         randomDifficulty = Math.floor(Math.random() * ((blueCards.length - 1) - 0 + 1) + 0);
+//         randomCard = Math.floor(Math.random() * (((blueCards[randomDifficulty]).length - 1) - 0 + 1) + 0);
+//         if (blueCards[randomDifficulty].length != 0) {
+//             cards.style.backgroundImage = `url(${(blueCards[randomDifficulty])[randomCard]}`;
+//             newDeck.push((blueCards[randomDifficulty])[randomCard]);
+//             blueCards[randomDifficulty].splice((blueCards[randomDifficulty])[randomCard], 1);
+//             console.log(blueCards[randomDifficulty])
+//         }
+//     }
+//     console.log(newDeck)
+// })
+
+
+
+// Changes after clicks
+azathoth.addEventListener('click', () => {
+    normal.style.visibility = 'visible'
+})
+
+normal.addEventListener('click', () => {
+    shuffle.style.visibility = 'visible'
+    normal.style.backgroundColor = 'black'
+    difficulty = 'normal';
+})
+
+shuffle.addEventListener('click', () => {
+    // shuffle.style.visibility = 'hidden'
+    shuffle.style.display = 'none'
+    stages.style.visibility = 'visible'
+    bcgCard.style.visibility = 'visible'
+})
+
+shuffle.addEventListener('click', () => {
+    // shuffle.style.visibility = 'hidden'
+    shuffle.style.display = 'none'
+    stages.style.visibility = 'visible'
+    bcgCard.style.visibility = 'visible'
+})
+
+
